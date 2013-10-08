7f99635c5d378d3e1da769b61db57bf48aa47e5c
Brian McDonnell <mcdonnellb@gmail.com>
2013-10-08 13:42:25 +0100

    Add url for hello view.

--- django_hello/hello/urls.py
+++ django_hello/hello/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -8,6 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hello.views.home', name='home'),
     # url(r'^hello/', include('hello.foo.urls')),
+    url(r'^hello/?', views.hello),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
