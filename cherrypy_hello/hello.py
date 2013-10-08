143bab6f32fb8af6adcb318da26e9fd9104215ce
Brian McDonnell <mcdonnellb@gmail.com>
2013-10-09 00:02:56 +0100

    Fix to allow cherrypy to work under nginx subdir behind uwsgi.

--- cherrypy_hello/hello.py
+++ cherrypy_hello/hello.py
@@ -9,11 +9,12 @@ class Handler:
     r.headers['Content-Type'] = 'text/plain'
     return "Hello from CherryPy"
 
-application = cherrypy.Application(Handler(), '/', None)
+application = cherrypy.Application(Handler(), '/cherrypy_hello', None)
 
 if __name__ == "__main__":
     import cherrypy.wsgiserver
-    server = cherrypy.wsgiserver.CherryPyWSGIServer(('127.0.0.1', 8000), application, 1)
+    wsgi_application = cherrypy.Application(Handler(), '/', None)
+    server = cherrypy.wsgiserver.CherryPyWSGIServer(('127.0.0.1', 8000), wsgi_application, 1)
     try:
         server.start()
     except KeyboardInterrupt:
