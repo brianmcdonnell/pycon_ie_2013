4a03c2fb2bb9e2b3a28ec992c9bb18cf727cc3f0
Brian McDonnell <mcdonnellb@gmail.com>
2013-10-09 17:05:42 +0100

    Node listening on unix socket

--- node_hello/server.js
+++ node_hello/server.js
@@ -5,5 +5,7 @@ app.get('/hello', function(req, res){
   res.send('Hello from Nodejs via (expressjs)');
 });
 
-app.listen(8080);
-console.log('Node listening on port 8080');
+//app.listen(8080);
+//console.log('Node listening on port 8080');
+app.listen('/tmp/node_hello.sock');
+console.log('Node listening at unix://tmp/node_hello.sock');
