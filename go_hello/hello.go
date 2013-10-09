9703170ca89c05b1d96cc72b70cbaaf2fecf8aa5
Brian McDonnell <mcdonnellb@gmail.com>
2013-10-09 18:38:33 +0100

    go_hello now listens on unix socket.

--- go_hello/hello.go
+++ go_hello/hello.go
@@ -11,5 +11,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/hello", handler)
-    http.ListenAndServe(":8080", nil)
+    //http.ListenAndServe(":8080", nil)
+    l, err := net.Listen("unix", "/tmp/go_hello.sock")
+    if err != nil {
+        fmt.Printf("%s\n", err)
+    } else {
+        err := http.Serve(l, nil)
+    }
 }
