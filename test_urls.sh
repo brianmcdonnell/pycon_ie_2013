9e56b2fd2634ab6644747f41fd25547a24045ec9
Brian McDonnell <mcdonnellb@gmail.com>
2013-10-10 16:09:53 +0100

    Script to curl each test url.

---
+++ test_urls.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+curl http://server.pycon.ie/hello
+curl http://server.pycon.ie/a.gif
+curl http://server.pycon.ie/http_hello
+curl http://server.pycon.ie/fastcgi_hello
+curl http://server.pycon.ie/uwsgi_hello
+curl http://server.pycon.ie/uwsgi_unix_hello
+curl http://server.pycon.ie/django_hello
+curl http://server.pycon.ie/django_min_hello
+curl http://server.pycon.ie/flask_hello
+curl http://server.pycon.ie/bottle_hello
+curl http://server.pycon.ie/cherrypy_hello
+curl http://server.pycon.ie/twisted_hello
+curl http://server.pycon.ie/tornado_hello
+curl http://server.pycon.ie/node_hello
+curl http://server.pycon.ie/go_hello
