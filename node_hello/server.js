7b17c77bbbc105d07f6f14ecc54196dd46a4046b
Brian McDonnell <mcdonnellb@gmail.com>
2013-10-11 12:37:07 +0100

    Minor enhancement to node hello output.

--- node_hello/server.js
+++ node_hello/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/hello', function(req, res){
-  res.send('Hello from Nodejs via (expressjs)');
+  res.send('Hello from Nodejs (via expressjs)');
 });
 
 
